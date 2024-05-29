3567756dc1e26ad0d7e112651dce7ced5ef719d4
srhqmp <ballanosarah@gmail.com>
2024-05-29 16:44:41 +0800

    feat: fetch all persons

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,8 +1,7 @@
 import ReactDOM from "react-dom/client";
 import {
   ApolloClient,
-  InMemoryCache,
-  gql,
+  InMemoryCache, 
   ApolloProvider,
 } from "@apollo/client";
 
@@ -11,24 +10,6 @@ import App from "./App.jsx";
 const client = new ApolloClient({
   uri: "http://localhost:4000",
   cache: new InMemoryCache(),
-});
-
-const query = gql`
-  query {
-    allPersons {
-      name
-      phone
-      address {
-        street
-        city
-      }
-      id
-    }
-  }
-`;
-
-client.query({ query }).then((response) => {
-  console.log(response.data);
 });
 
 ReactDOM.createRoot(document.getElementById("root")).render(
