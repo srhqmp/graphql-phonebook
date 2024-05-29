73f8bc000d08769002be665532570aec75c0c037
srhqmp <ballanosarah@gmail.com>
2024-05-29 17:32:31 +0800

    feat(client): implement editing of phone number

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import { useQuery } from "@apollo/client";
 
 import Persons from "./components/Persons";
 import PersonForm from "./components/PersonForm";
+import PhoneForm from "./components/PhoneForm";
 import Notify from "./components/Notify";
 
 import { ALL_PERSONS } from "./queries";
@@ -25,8 +26,9 @@ const App = () => {
   return (
     <div>
       <Notify errorMessage={errorMessage} />
-      <PersonForm setError={notify} />
       <Persons persons={result.data.allPersons} />
+      <PersonForm setError={notify} />
+      <PhoneForm setError={notify} />
     </div>
   );
 };
