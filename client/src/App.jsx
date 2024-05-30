20aa9274ca2b9326dbef5a262a0591ed0f850c19
srhqmp <ballanosarah@gmail.com>
2024-05-30 17:39:07 +0800

    feat(client): add user login

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,20 +1,26 @@
-import { useState } from "react";
-import { useQuery } from "@apollo/client";
+import { useEffect, useState } from "react";
+import { useApolloClient, useQuery } from "@apollo/client";
 
 import Persons from "./components/Persons";
 import PersonForm from "./components/PersonForm";
 import PhoneForm from "./components/PhoneForm";
 import Notify from "./components/Notify";
+import LoginForm from "./components/LoginForm";
 
 import { ALL_PERSONS } from "./queries";
 
 const App = () => {
+  const [token, setToken] = useState(null);
   const [errorMessage, setErrorMessage] = useState(null);
   const result = useQuery(ALL_PERSONS);
+  const client = useApolloClient();
 
-  if (result.loading) {
-    return <div>loading...</div>;
-  }
+  useEffect(() => {
+    const storage_token = localStorage.getItem("phonenumbers-user-token");
+    if (storage_token) {
+      setToken(storage_token);
+    }
+  }, []);
 
   const notify = (message) => {
     setErrorMessage(message);
@@ -23,9 +29,30 @@ const App = () => {
     }, 10000);
   };
 
+  const logout = () => {
+    setToken(null);
+    localStorage.clear();
+    client.resetStore();
+  };
+
+  if (!token) {
+    return (
+      <div>
+        <Notify errorMessage={errorMessage} />
+        <h2>Login</h2>
+        <LoginForm setToken={setToken} setError={notify} />
+      </div>
+    );
+  }
+
+  if (result.loading) {
+    return <div>loading...</div>;
+  }
+
   return (
     <div>
       <Notify errorMessage={errorMessage} />
+      <button onClick={logout}>logout</button>
       <Persons persons={result.data.allPersons} />
       <PersonForm setError={notify} />
       <PhoneForm setError={notify} />
