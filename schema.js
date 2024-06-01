add3a9e1b7bae77660ed2acb4edce4f7e30db205
srhqmp <ballanosarah@gmail.com>
2024-06-01 14:15:09 +0800

    refactor: move resolvers and schema to own modules

---
+++ schema.js
@@ -0,0 +1,60 @@
+const typeDefs = `
+  type User {
+    username: String!
+    friends: [Person!]!
+    id: ID!
+  }
+
+  type Token {
+    value: String!
+  }
+
+  enum YesNo {
+    YES
+    NO
+  }
+  
+  type Address {
+    street: String!
+    city: String!
+  }
+
+  type Person {
+    name: String!
+    phone: String
+    address: Address!
+    id: ID!
+  }
+  
+  type Query {
+    personCount: Int!
+    allPersons(phone: YesNo): [Person!]!
+    findPerson(name: String!): Person
+    me: User
+  }
+
+  type Mutation {
+    createUser(
+      username: String!
+    ): User
+    login(
+      username: String!
+      password: String!
+    ): Token
+    addPerson(
+      name: String!
+      phone: String
+      street: String!
+      city: String!
+    ): Person
+    addAsFriend(
+      name: String!
+    ): User
+    editNumber(
+      name: String!
+      phone: String!
+    ): Person
+  }
+`;
+
+module.exports = typeDefs;
